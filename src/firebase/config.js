30918a765858916c7497f9b74115cb911881b619
Posoroko <eric@posoroko.com>
2022-08-31 11:03:28 +0200

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAIwI0ZJKiwiP20x19Kc4afqBGcD4RSw1c",
+    authDomain: "edgard-search.firebaseapp.com",
+    projectId: "edgard-search",
+    storageBucket: "edgard-search.appspot.com",
+    messagingSenderId: "769757005",
+    appId: "1:769757005:web:0dc476bf9d06b19f14b104",
+    measurementId: "G-CWEJ92J2WP"
+  };
+  
+initializeApp(firebaseConfig)
+
+
+const firestore = getFirestore()
+
+export { firestore }
